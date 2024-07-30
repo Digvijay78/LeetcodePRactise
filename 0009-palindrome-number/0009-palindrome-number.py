e498e4a5c68197f982fc82092edde02de07faa09
Digvijay Singh <singhdigvijay1605@gmail.com>
2024-07-30 23:27:41 +0530

    Time: 57 ms (43.4%), Space: 16.7 MB (6.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        lis = []
+        while x >= 10:
+            lis.append(x % 10)
+            x //= 10
+        lis.append(x)
+        for i in range(int(math.ceil(len(lis)/2))):
+            if lis[i] != lis[len(lis)-i-1]:
+                return False
+        return True
