87caa70e794fc813fb1b9789de692c5a11953951
Digvijay Singh <singhdigvijay1605@gmail.com>
2024-12-29 13:05:00 +0530

    Time: 2 ms (84.99%), Space: 45.9 MB (50.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,24 +1,18 @@
 class Solution {
 
-     public void adjustZeros(int[] nums){
-        
-        int count =0;
-       for(int i=0; i<nums.length ; i++){
-
-        if(nums[i] != 0){
-           
-           int temp = nums[i];
-          nums[i] = 0;
-          nums[count] = temp;
-          count++;
-       }
-       }
-      
-     }
+   
 
     public void moveZeroes(int[] nums) {
         
-        
-        adjustZeros(nums);
+        int index = 0;
+        for(int i=0; i< nums.length ; i++){
+
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] =0;
+                nums[index] = temp;
+                index++;
+            }
+        }
     }
 }
